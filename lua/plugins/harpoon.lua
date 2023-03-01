d5331309298b178294cf955ec9468b354746aa98
Nicholas Michlin <nicholas.hy.michlin@gmail.com>
2023-03-02 00:28:18 +0800

    fix harpoon

--- lua/plugins/harpoon.lua
+++ lua/plugins/harpoon.lua
@@ -1,7 +1,6 @@
 return {
   "ThePrimeagen/harpoon",
   keys = function()
-    local ui = require("harpoon.ui")
     return {
       {
         "<leader>ha",
@@ -13,42 +12,42 @@ return {
       {
         "<leader>hh",
         function()
-          ui.toggle_quick_menu()
+          require("harpoon.ui").toggle_quick_menu()
         end,
         desc = "Menu",
       },
       {
         "<leader>h<tab>",
         function()
-          ui.nav_next()
+          require("harpoon.ui").nav_next()
         end,
         desc = "Next mark",
       },
       {
         "<leader>h1",
         function()
-          ui.nav_file(1)
+          require("harpoon.ui").nav_file(1)
         end,
         desc = "Mark 1",
       },
       {
         "<leader>h2",
         function()
-          ui.nav_file(2)
+          require("harpoon.ui").nav_file(2)
         end,
         desc = "Mark 2",
       },
       {
         "<leader>h3",
         function()
-          ui.nav_file(3)
+          require("harpoon.ui").nav_file(3)
         end,
         desc = "Mark 3",
       },
       {
         "<leader>h4",
         function()
-          ui.nav_file(4)
+          require("harpoon.ui").nav_file(4)
         end,
         desc = "Mark 4",
       },
--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -1,5 +1,8 @@
 return {
   "nvim-telescope/telescope.nvim",
+  init = function()
+    require("telescope").load_extension("harpoon")
+  end,
   cmd = "Telescope",
   keys = { { "<leader>hm", "<cmd>Telescope harpoon marks<cr>", desc = "Preview marks" } },
 }
