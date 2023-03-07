1bc08e0bde61cc652abf78d75b30b692d9909c64
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-03-07 22:43:17 +0800

    cursor always in the middle of the page when using N and n

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -11,3 +11,7 @@ map.set("v", "J", ":m '>+1<CR>gv=gv")
 -- allow half page jumps and center cursor in middle of page
 map.set("n", "<C-d>", "<C-d>zz")
 map.set("n", "<C-u>", "<C-u>zz")
+
+-- cursor in the middle of the page during searching
+map.set("n", "n", "nzz")
+map.set("n", "N", "Nzz")
