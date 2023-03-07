f25509cd874dc5233bb148771c015a5253df6d1d
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-03-07 08:40:30 +0800

    new keymaps

--- lua/config/keymaps.lua
+++ lua/config/keymaps.lua
@@ -3,3 +3,11 @@
 -- Add any additional keymaps here
 local map = vim.keymap
 map.set("i", "jk", "<Esc>")
+
+-- move highlighted text up and down
+map.set("v", "K", ":m '<-2<CR>gv=gv")
+map.set("v", "J", ":m '>+1<CR>gv=gv")
+
+-- allow half page jumps and center cursor in middle of page
+map.set("n", "<C-d>", "<C-d>zz")
+map.set("n", "<C-u>", "<C-u>zz")
