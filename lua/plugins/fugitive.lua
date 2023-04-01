346d012e6c816766ea000c913fd49803804209dc
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-04-01 12:37:43 +0800

    correct key mappings

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,8 +3,8 @@ return {
   keys = function()
     return {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
-      { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git" },
-      { "<leader>gP", "<cmd>Git push<cr>", desc = "Git" },
+      { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git pull" },
+      { "<leader>gP", "<cmd>Git push<cr>", desc = "Git push" },
     }
   end,
 }
