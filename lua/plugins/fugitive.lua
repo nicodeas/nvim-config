b08a71868f793cdd2dc47c7f2b3959a16b3eab97
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-04-01 12:35:02 +0800

    add keymaps to push and pull for fugitive

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -3,6 +3,8 @@ return {
   keys = function()
     return {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
+      { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git" },
+      { "<leader>gP", "<cmd>Git push<cr>", desc = "Git" },
     }
   end,
 }
