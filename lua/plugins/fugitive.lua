89b05003cfd98980828c031c3f7eed801facd2d4
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-04-01 12:53:00 +0800

    feat: fugitive toggle git blame pannel

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -5,6 +5,7 @@ return {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
       { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git pull" },
       { "<leader>gP", "<cmd>Git push<cr>", desc = "Git push" },
+      { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame" },
     }
   end,
 }
