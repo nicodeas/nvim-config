6126964530c34d78cfad93acc25872404d4f4b98
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-04-01 13:03:05 +0800

    Add git diff keymapping

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -6,6 +6,7 @@ return {
       { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git pull" },
       { "<leader>gP", "<cmd>Git push<cr>", desc = "Git push" },
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame side panel" },
+      { "<leader>gd", "<cmd>Git diff<cr>", desc = "Git diff" },
     }
   end,
 }
