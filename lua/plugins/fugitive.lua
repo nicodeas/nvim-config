90099dab38b3be487edbc86426b9b4ba392569d1
Nicholas Michlin <nicholas.hy.michlin@gmail.com>
2023-04-13 09:56:35 +0800

    git push keymapping automatically sets upstream

--- lua/plugins/fugitive.lua
+++ lua/plugins/fugitive.lua
@@ -4,7 +4,7 @@ return {
     return {
       { "<leader>gg", "<cmd>Git<cr>", desc = "Git" },
       { "<leader>gp", "<cmd>Git pull<cr>", desc = "Git pull" },
-      { "<leader>gP", "<cmd>Git push<cr>", desc = "Git push" },
+      { "<leader>gP", "<cmd>Git -c push.default=current push<cr>", desc = "Git push" },
       { "<leader>gb", "<cmd>Git blame<cr>", desc = "Git blame side panel" },
       { "<leader>gd", "<cmd>Git diff<cr>", desc = "Git diff" },
       { "<leader>gl", "<cmd>Git log<cr>", desc = "Git log" },
