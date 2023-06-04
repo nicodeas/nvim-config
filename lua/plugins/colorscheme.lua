da27b07c43e97afaac599299a90673fd47bb3c7a
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-06-05 00:29:24 +0800

    update colorscheme.lua

---
+++ lua/plugins/colorscheme.lua
@@ -0,0 +1,13 @@
+return {
+  -- uncomment colorscheme you want to use
+  -- { "ellisonleao/gruvbox.nvim" },
+  -- { "rose-pine/neovim", name = "rose-pine" },
+  { "sainnhe/everforest" },
+  -- update colorscheme below
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "everforest",
+    },
+  },
+}
