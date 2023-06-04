2df77055b94c4d046881ab7a80f7e9652a50a117
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-06-05 00:34:02 +0800

    update toggle term bindings to include lazygit terminal

--- lua/plugins/toggle-term.lua
+++ lua/plugins/toggle-term.lua
@@ -3,14 +3,27 @@ return {
   version = "*",
   config = true,
   keys = function()
+    local Terminal = require("toggleterm.terminal").Terminal
+    local lazygit = Terminal:new({ cmd = "lazygit", hidden = true, direction = "float" })
+    local localTerminal = Terminal:new({ hidden = true, direction = "float" })
+
+    function LazygitToggle()
+      lazygit:toggle()
+    end
+
+    function LocalTerminalToggle()
+      localTerminal:toggle()
+    end
+
     return {
       {
         "<leader>t",
-        "<cmd>ToggleTerm direction=float<CR>",
+        "<cmd>lua LocalTerminalToggle()<CR>",
         desc = "Terminal",
         mode = "n",
       },
       { "qq", "<cmd>close<cr>", mode = "t" },
+      { "<leader>lg", "<cmd>lua LazygitToggle()<CR>" },
     }
   end,
 }
