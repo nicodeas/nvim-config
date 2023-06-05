0361223dd91d9e412ef0b5feba103f16df3f19ee
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-06-05 11:31:59 +0800

    colorscheme updates

--- lua/plugins/colorscheme.lua
+++ lua/plugins/colorscheme.lua
@@ -1,7 +1,45 @@
 return {
-  -- uncomment colorscheme you want to use
-  -- { "ellisonleao/gruvbox.nvim" },
-  -- { "rose-pine/neovim", name = "rose-pine" },
+  -- set lazy to false to include in ui options
+  { "rose-pine/neovim", name = "rose-pine", lazy = true },
+  {
+    "folke/tokyonight.nvim",
+    lazy = true,
+    opts = { style = "moon" },
+  },
+  {
+    "catppuccin/nvim",
+    lazy = true,
+    name = "catppuccin",
+    opts = {
+      integrations = {
+        alpha = true,
+        cmp = true,
+        gitsigns = true,
+        illuminate = true,
+        indent_blankline = { enabled = true },
+        lsp_trouble = true,
+        mini = true,
+        native_lsp = {
+          enabled = true,
+          underlines = {
+            errors = { "undercurl" },
+            hints = { "undercurl" },
+            warnings = { "undercurl" },
+            information = { "undercurl" },
+          },
+        },
+        navic = { enabled = true },
+        neotest = true,
+        noice = true,
+        notify = true,
+        nvimtree = true,
+        semantic_tokens = true,
+        telescope = true,
+        treesitter = true,
+        which_key = true,
+      },
+    },
+  },
   { "sainnhe/everforest" },
   -- update colorscheme below
   {
