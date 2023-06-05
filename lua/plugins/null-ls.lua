fdeb3ee633df2fe84cfa3f5ebe34eb87f6615c1a
Nicodeas <nicholas.hy.michlin@gmail.com>
2023-06-05 14:22:06 +0800

    update flake8 to ignore unused import in __init__ files

--- lua/plugins/null-ls.lua
+++ lua/plugins/null-ls.lua
@@ -7,7 +7,9 @@ return {
     return {
       sources = {
         nls.builtins.formatting.black,
-        nls.builtins.diagnostics.flake8.with({ extra_args = { "--ignore=E501,E203,W503,W504" } }),
+        nls.builtins.diagnostics.flake8.with({
+          extra_args = { "--ignore=E501,E203,W503,W504", "--per-file-ignores=__init__.py:F401" },
+        }),
         nls.builtins.formatting.stylua,
         nls.builtins.formatting.prettierd,
         nls.builtins.formatting.google_java_format,
