40dc23b757a5e3c98ed6751d79ba3b3df7e11a8b
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-23 14:42:59 +0800

    opacity of popup window

--- lua/plugins/installed/dressing.lua
+++ lua/plugins/installed/dressing.lua
@@ -1,4 +1,11 @@
 return {
   "stevearc/dressing.nvim",
-  event="VeryLazy",
+  event = "VeryLazy",
+  opts = {
+    input = {
+      win_options = {
+        winblend = 0,
+      },
+    },
+  },
 }
