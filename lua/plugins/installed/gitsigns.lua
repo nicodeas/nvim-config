71028f48fa9a19e1abd248b6239a77b87e1e9d22
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-25 10:37:09 +0800

    add gitsigns plugin

---
+++ lua/plugins/installed/gitsigns.lua
@@ -0,0 +1,38 @@
+return {
+  "lewis6991/gitsigns.nvim",
+  event = { "BufReadPre", "BufNewFile" },
+  opts = {
+    on_attach = function(bufnr)
+      local gs = package.loaded.gitsigns
+
+      local function map(mode, l, r, opts)
+        opts = opts or {}
+        opts.buffer = bufnr
+        vim.keymap.set(mode, l, r, opts)
+      end
+      -- mappings to jump to hunks
+      map("n", "]h", function()
+        if vim.wo.diff then
+          return "]h"
+        end
+        vim.schedule(function()
+          gs.next_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true })
+
+      map("n", "[h", function()
+        if vim.wo.diff then
+          return "[h"
+        end
+        vim.schedule(function()
+          gs.prev_hunk()
+        end)
+        return "<Ignore>"
+      end, { expr = true })
+      -- keymaps
+      map("n", "<leader>hs", gs.stage_hunk)
+      map("n", "<leader>hr", gs.reset_hunk)
+    end,
+  },
+}
