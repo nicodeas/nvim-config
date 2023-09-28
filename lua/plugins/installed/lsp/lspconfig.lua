421964f02743b59e88610774e4d300c98339c033
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-28 13:01:14 +0800

    update lsp config

--- lua/plugins/installed/lsp/lspconfig.lua
+++ lua/plugins/installed/lsp/lspconfig.lua
@@ -1,3 +1,63 @@
+local on_attach = function(_, bufnr)
+  vim.keymap.set("n", "[d", vim.diagnostic.goto_prev, { desc = "Goto previous diagnostic message" })
+  vim.keymap.set("n", "]d", vim.diagnostic.goto_next, { desc = "Goto next diagnostic message" })
+
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
+
+  nmap("<leader>rn", vim.lsp.buf.rename, "[R]e[n]ame")
+  nmap("<leader>ca", vim.lsp.buf.code_action, "[C]ode [A]ction")
+  nmap("gd", vim.lsp.buf.definition, "[G]oto [D]efinition")
+  nmap("gr", require("telescope.builtin").lsp_references, "[G]oto [R]eferences")
+  nmap("<leader>D", vim.lsp.buf.type_definition, "Type [D]efinition")
+
+  -- See `:help K` for why this keymap
+  nmap("K", vim.lsp.buf.hover, "Hover Documentation")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+
+  nmap("<leader>rs", ":LspRestart<Cr>", "[R]e[S]tart")
+
+  -- Create a command `:Format` local to the LSP buffer
+  vim.api.nvim_buf_create_user_command(bufnr, "Format", function(_)
+    vim.lsp.buf.format()
+  end, { desc = "Format current buffer with LSP" })
+
+end
+
+local servers = {
+  lua_ls = {
+    Lua = {
+      diagnostics = { globals = { "vim" } },
+    },
+    workspace = {
+      library = {
+        [vim.fn.expand("$VIMRUNTIME/lua")] = true,
+        [vim.fn.stdpath("config") .. "/lua"] = true,
+      },
+    },
+  },
+  pyright = {
+    python = {
+      analysis = {
+        typeCheckingMode = "off",
+        diagnosticMode = "openFilesOnly",
+        autoSearchPaths = true,
+        useLibraryCodeFortypes = true,
+      },
+    },
+  },
+  volar = {},
+  tsserver = {},
+  emmet_ls = {},
+  clangd = {},
+  dockerls = {},
+  tailwindcss = {},
+}
+
 return {
   "neovim/nvim-lspconfig",
   event = { "BufReadPre", "BufNewFile" },
