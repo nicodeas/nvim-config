7861dba5a358ca4f3153135e51ae7cf4c045fd4b
nicodeas <nicholas.hy.michlin@gmail.com>
2023-09-30 12:02:59 +0800

    update telescope keymaps

--- lua/plugins/installed/telescope.lua
+++ lua/plugins/installed/telescope.lua
@@ -9,6 +9,7 @@ return {
   config = function()
     local telescope = require("telescope")
     local actions = require("telescope.actions")
+    local builtin = require("telescope.builtin")
     telescope.setup({
       defaults = {
         path_display = { "truncate " },
@@ -24,8 +25,9 @@ return {
     })
     telescope.load_extension("fzf")
     local keymap = vim.keymap
-    keymap.set("n", "<leader><leader>", "<cmd>Telescope find_files<cr>", { desc = "Fuzzy find files in cwd" })
-    keymap.set("n", "<leader>sg", "<cmd>Telescope live_grep<cr>", { desc = "Find string in cwd" })
-    keymap.set("n", "<leader>*", "<cmd>Telescope grep_string<cr>", { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader><leader>", builtin.find_files, { desc = "Fuzzy find files in cwd" })
+    keymap.set("n", "<leader>sg", builtin.live_grep, { desc = "Find string in cwd" })
+    keymap.set("n", "<leader>*", builtin.grep_string, { desc = "Find string under cursor in cwd" })
+    keymap.set("n", "<leader>tr", builtin.resume, { desc = "[T]elescope [R]esume" })
   end,
 }
