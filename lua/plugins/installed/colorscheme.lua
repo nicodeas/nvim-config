463707229dc323cbc2b5bf9c3c963a5ce5732ad2
nicodeas <nicholas.hy.michlin@gmail.com>
2024-02-12 09:15:27 +0800

    update colorscheme

--- lua/plugins/installed/colorscheme.lua
+++ lua/plugins/installed/colorscheme.lua
@@ -3,7 +3,29 @@ return {
     "rebelot/kanagawa.nvim",
     name = "kanagawa",
     priority = 1000,
-    config = function()
+    opts = {
+      transparent = true,
+      colors = {
+        theme = {
+          all = {
+            ui = {
+              bg_gutter = "none",
+            },
+          },
+        },
+      },
+      overrides = function(colors)
+        local theme = colors.theme
+        return {
+          NormalFloat = { bg = "none" },
+          FloatBorder = { bg = "none" },
+          FloatTitle = { bg = "none" },
+          NormalDark = { fg = theme.ui.fg_dim, bg = theme.ui.bg_m3 },
+        }
+      end,
+    },
+    config = function(_, opts)
+      require("kanagawa").setup(opts)
       vim.cmd("colorscheme kanagawa")
     end,
   },
